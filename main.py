580a11fa2e662aed9b35415123e3ef71c5aca987
romoch82 <romoch37@gmail.com>
2024-12-01 22:28:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
 
-api = '7217501846:AAF0*...*'
+api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
